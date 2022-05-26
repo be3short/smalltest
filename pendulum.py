73a4c7dfe836d656b9ee3e34a7133faf0a777508
be3short <beshort@ucsc.edu>
2022-05-26 15:21:27 -0700

    added json

--- pendulum.py
+++ pendulum.py
@@ -23,24 +23,25 @@ def C(x,hs):
     return True
 
 
-if __name__ == "__main__":
-
-    system = osdt.create_system(State(angle=1.5, velocity=0.0),f=F, vars={Params:Params(length=1.0, mass=1.0, gravity=9.81)})
+#if __name__ == "__main__":
+def main():
+    osdt.clear()
+    system = create()
 
     # run the simulation
-    osdt.run(t=10.0, j=20)
+    osdt.run(time=10.0, jumps=20)
 
     # create a figure
-    fig = osdt.create_figure(layout=[[1],[2]],
+    fig = osdt.create_fig(layout=[[1],[2]],
                                  title="Pendulum",
                                  width=1600, height=600)
 
     # plot the angle and velocity
-    fig.sub(1).plot("angle")
-    fig.sub(2).plot("velocity")
-
+    fig.plot(1,"angle")
+    fig.plot(2,"velocity")
+    osdt.plotting.display()
     # display all figures
-    osdt.display()
+   # osdt.display()
 
 
 
