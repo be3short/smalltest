4e77347c2aa6d6578d49d1b9b61ef848b6b6a3e2
be3short <beshort@ucsc.edu>
2023-07-26 21:29:21 -0700

    Create __lib__osdt_L20230704YISdUGaDCn0Ko4GVChvLIW0.py

---
+++ __lib__osdt_L20230704YISdUGaDCn0Ko4GVChvLIW0.py
@@ -0,0 +1,23 @@
+"""============================ osdt library ==================================
+
+Use the id below to reference this library:
+
+L20230704YISdUGaDCn0Ko4GVChvLIW0
+
+===============================================================================
+
+Define dependencies in this file using osdt.lib with an id, for example this 
+library could be included as a dependency in another library as follows:
+
+-------------------------------------------------------------------------------
+import osdt
+library = osdt.lib(id='L20230704YISdUGaDCn0Ko4GVChvLIW0')
+-------------------------------------------------------------------------------
+
+Modules can then be imported from this library using the 'lib' prefix with 
+the defined library name and the regular import syntax:
+
+-------------------------------------------------------------------------------
+from lib.library import module
+-------------------------------------------------------------------------------
+"""
